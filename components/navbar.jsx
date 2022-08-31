155716323bdcccdeb2b74d5de51295e45d61ab4d
tawakalmit <tawakal.mit@gmail.com>
2022-08-31 13:27:51 +0700

    fix homepage

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar({about, skill, education, certificate, projects, logodesign}) {
   return (
-    <div className="w-8/12 h-10 flex items-center justify-around bg-white mx-auto mt-2 rounded-xl px-5 shadow-md">
+    <div className="w-11/12 h-fit flex flex-wrap items-center justify-around bg-white mx-auto mt-2 rounded-xl px-5 shadow-md md:w-8/12 p-3">
         <p className={about}>About</p>
         <p className={skill}>Skill</p>
         <p className={education}>Education</p>
