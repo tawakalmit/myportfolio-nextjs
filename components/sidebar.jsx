3aa94f2b99c5686ea7248621abc26ad7222127f0
tawakalmit <tawakal.mit@gmail.com>
2022-08-31 13:08:56 +0700

    homepage

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -9,14 +9,14 @@ import { useRouter } from 'next/router'
 export default function Sidebar() {
     const router = useRouter();
   return (
-    <div className='w-full h-14 md:w-4/12 min-h-screen bg-[#2c3e50]'>
+    <div className='md:w-full min-h-screen bg-[#2c3e50]'>
         <div className='w-10/12 h-auto md:w-10/12 py-20 mx-auto flex flex-col items-center'>
-            <div className='w-fit mx-auto'>
+            <div className='w-24 md:w-fit mx-auto'>
             <Image src="/profile.jpg" width={200} height={200} alt="profile" className='rounded-full' />
             </div>
-            <h1 className='text-[#ecf0f1] font-semibold text-2xl mt-5'>M Ikbal Tawakal</h1>
-            <h2 className='text-white opacity-50'>Front End Engineer</h2>
-            <p className='text-white text-sm opacity-80'>tawakal.mit@gmail.com</p>
+            <h1 className='text-[#ecf0f1] font-semibold md:text-2xl mt-5'>M Ikbal Tawakal</h1>
+            <h2 className='text-sm text-white opacity-50 md:text-xl'>Front End Engineer</h2>
+            <p className='text-white text-xs md:text-sm opacity-80'>tawakal.mit@gmail.com</p>
             <div className='w-11/12 flex justify-around mt-10'>
                 <FaLinkedin color='#ecf0f1' size={30} alt="Linkedin" onClick={() => router.push("https://www.linkedin.com/in/mikbaltawakal29/")} className='cursor-pointer hover:opacity-40' />
                 <FaGithub color='#ecf0f1' size={30} alt="Github" onClick={() => router.push("https://github.com/tawakalmit")} className='cursor-pointer hover:opacity-40' />
