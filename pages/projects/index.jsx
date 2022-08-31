002c042f3f3eab043eb8eb56abb93ec2902d71a6
tawakalmit <tawakal.mit@gmail.com>
2022-08-31 14:49:40 +0700

    education

---
+++ pages/projects/index.jsx
@@ -0,0 +1,22 @@
+import Layout from "../../components/layout"
+
+export default function Projects() {
+  return (
+    <>
+    <Layout
+    page="Projects"
+    about="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
+    skill="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
+    education="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
+    certificate="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
+    projects="text-[#3498db] cursor-pointer"
+    logodesign="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
+    content={
+        <div>
+
+        </div>
+    }
+    />  
+    </>
+  )
+}
