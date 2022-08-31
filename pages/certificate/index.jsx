61afa779565b79b3ac3848f2f6c7cc4a9cc554f5
tawakalmit <tawakal.mit@gmail.com>
2022-08-31 15:51:38 +0700

    certificate

--- pages/certificate/index.jsx
+++ pages/certificate/index.jsx
@@ -1,4 +1,5 @@
 import Layout from "../../components/layout"
+import Image from "next/dist/client/image"
 
 export default function Certificate() {
   return (
@@ -12,8 +13,9 @@ export default function Certificate() {
     projects="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
     logodesign="cursor-pointer opacity-50 hover:opacity-100 active:text-[#3498db]"
     content={
-        <div>
-
+        <div className="w-11/12 mx-auto flex justify-around mt-5 md:mt-20">
+            <Image src="/webdasar.png" width={400} height={300} alt="certi" className="m-3" />
+            <Image src="/frontend.png" width={400} height={300} alt="certi" className="m-3" />
         </div>
     }
     />  
